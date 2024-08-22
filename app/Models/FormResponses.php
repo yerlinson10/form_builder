af38071908f01e991bc4f6b30ff89fe4b762d818
yerlinson <53019615+yerlinson10@users.noreply.github.com>
2024-08-22 16:16:52 -0400

    Update FormResponses.php

--- app/Models/FormResponses.php
+++ app/Models/FormResponses.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class form_responses extends Model
+class FormResponses extends Model
 {
     use HasFactory;
 }
