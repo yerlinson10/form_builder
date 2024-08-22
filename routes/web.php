78a260e4d3ad23cced8ebee1067f34fe42483863
yerlinson <53019615+yerlinson10@users.noreply.github.com>
2024-08-22 16:15:00 -0400

    feat: Create routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\FormsController;
+use App\Http\Controllers\FormResponsesController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -14,4 +16,8 @@ Route::middleware([
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
+
+    Route::resource('forms', FormsController::class);
+    Route::post('/forms/{form}/submit', [FormsController::class, 'submitForm'])->name('forms.submit');
+    Route::resource('answers', FormResponsesController::class);
 });
